48706c3e4cbaf8e56d3b5adb8cf7169a395c041c
AnuraagSeenuvasan <112798792+AnuraagSeenuvasan@users.noreply.github.com>
2022-09-09 07:39:43 +0430

    Update Factorial.java

--- week1/day1/classroom/Factorial.java
+++ week1/day1/classroom/Factorial.java
@@ -37,11 +37,6 @@ public class Factorial {
 		
 
 		// Print factorial (fact)
-		int n = 5, output = 1;
-		for (int i = 1; i <= n; i++) {
-			output = output * i;
-		}
-		System.out.println(output);
 	
 	}
 
